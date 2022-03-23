b69f3db46b450781502d2da2025ffbb5cbb51482
Hisahito <hisahito706@gmail.com>
2022-03-23 15:54:22 -0600

    primer commit

---
+++ useForm.js
@@ -0,0 +1,26 @@
+//Custom Hook encargado de formularios
+import { useState } from "react";
+
+
+export const useForm = (initialState = {} ) => {
+
+    const [values, setValues] = useState(initialState)
+
+    const reset = () => {
+        setValues( initialState );
+    }
+
+    const handleImputChange = ({ target }) => {
+
+        setValues({
+            ...values,
+            [ target.name ]: target.value 
+        });
+
+    }
+
+    return [ values, handleImputChange, reset ];
+}
+
+
+
